eb0f364dc356d541587648d04cc214114445a213
Si Bartha <silas@exvacuum.dev>
2022-08-15 08:10:48 -0400

    Done lol

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use chrono::prelude::*;
+
+const FIXED_LUNAR_CYCLE: i64 = 2551443;
+
+const CYCLE_CHARS: [char; 28] = [
+    '','','','','','','',
+    '','','','','','','',
+    '','','','','','','',
+    '','','','','','',''
+];
+
+fn main() {
+    let start = Utc.ymd(2022, 7, 28).and_hms(17, 54, 00);
+    let now = Utc::now();
+
+    let duration = now - start;
+    println!("{}", duration.num_seconds());
+    let duration = duration.num_seconds() % FIXED_LUNAR_CYCLE;
+    let cycle_factor = duration as f64/FIXED_LUNAR_CYCLE as f64;
+    let cycle_char = (cycle_factor * CYCLE_CHARS.len() as f64) as usize;
+    print!("{}", CYCLE_CHARS[cycle_char]);
+}
